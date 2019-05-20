2f09791c002671750abdc337663e42b10e19dd54
Philip Top <top1@llnl.gov>
2019-05-20 16:42:26 -0700

    First cut at HELICS as a subproject for BBX

---
+++ src/helics-bbx.cpp
@@ -0,0 +1,8 @@
+/*
+Copyright © 2019,
+Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance for Sustainable Energy, LLC.  See
+the top-level NOTICE for additional details. All rights reserved.
+SPDX-License-Identifier: BSD-3-Clause
+*/
+
+int main (int argc, char *argv[]) { return 0; }
