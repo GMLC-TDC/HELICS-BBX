337a3b6915358f876102540d4871b553dd71cda9
Philip Top <top1@llnl.gov>
2019-11-14 11:21:07 -0800

    update bbx cmake to use HELICS 2.3 and actually instantiate a few functions in the main

--- src/helics-bbx.cpp
+++ src/helics-bbx.cpp
@@ -4,5 +4,29 @@ Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance
 the top-level NOTICE for additional details. All rights reserved.
 SPDX-License-Identifier: BSD-3-Clause
 */
+#include "helics/application_api.hpp"
+#include "helics/core/helicsCLI11.hpp"
 
-int main (int argc, char *argv[]) { return 0; }
+
+int main (int argc, char *argv[]) { 
+	helics::helicsCLI11App bbxcli;
+	auto result = bbxcli.helics_parse(argc, argv);
+	switch (result)
+	{
+		case helics::helicsCLI11App::parse_output::version_call:
+			break;
+		case helics::helicsCLI11App::parse_output::help_call:
+			case helics::helicsCLI11App::parse_output::help_all_call:
+				return 0;
+			default:
+				break;
+
+	}
+
+	helics::FederateInfo fi(bbxcli.remainArgs());
+	fi.defName = "bbxfed";
+	helics::CombinationFederate cFed(std::string(),fi);
+
+	cFed.disconnect();
+	
+}
